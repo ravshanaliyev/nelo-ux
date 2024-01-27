7c33d6d13bd77f708e256785fcc33d7d66f9856b
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-27 21:18:51 +0500

    pricing section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Navbar } from "./components/Navbar";
 import { Faq } from "./components/faq";
 import { Features } from "./components/features";
 import { Hero } from "./components/hero";
+import { Pricing } from "./components/pricing";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <div className="px-[20px] lg:container lg:px-20 mx-auto">
         <Features />
         <Faq />
+        <Pricing />
       </div>
     </>
   );
