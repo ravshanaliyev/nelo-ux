fe9017ed11c1ecff5a105873b4e9ba2bbb2384e8
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-27 21:03:00 +0500

    hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/hero";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
