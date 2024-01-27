3339968ee61102edead7b5361cc91956705af2be
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-27 21:07:15 +0500

    features section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "./components/Navbar";
+import { Features } from "./components/features";
 import { Hero } from "./components/hero";
 
 export default function Home() {
@@ -6,6 +7,9 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
+      <div className="px-[20px] lg:container lg:px-20 mx-auto">
+        <Features />
+      </div>
     </>
   );
 }
